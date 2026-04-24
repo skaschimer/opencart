0635fc24c1615d3f7458f006a37f37dc1463fa14
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-24 18:16:54 +0800

    update select all as a web component

--- upload/assets/framework/input/checkbox.js
+++ upload/assets/framework/input/checkbox.js
@@ -5,22 +5,13 @@ const language = loader.language('default');
 
 customElements.define('checkbox-all', class extends WebComponent {
     render() {
-        let html = '';
-
-        html += '<div class="form-check">';
-        html += '   <input type="checkbox" id="' + this.getAttribute('input-id') + '" class="form-check-input" data-on="change:onChange" data-target="' + this.getAttribute('target') + '"/>';
-        html += '   <label for="' + this.getAttribute('input-id') + '" class="form-check-label">' + this.innerHTML + '</label>';
-        html += '</div>';
-
-        return html;
+        return '<input type="checkbox" id="' + this.getAttribute('input-id') + '" class="form-check-input" data-on="change:onChange" data-target="' + this.getAttribute('target') + '"/>';
     }
 
     onChange(e) {
         let target = document.getElementById(e.target.getAttribute('data-target'));
 
-        const filtered = Array.from(target.querySelectorAll('input[type=\'checkbox\']')).filter(element => {
-            return !element.parentElement.parentElement.matches('checkbox-all');
-        });
+        const filtered = Array.from(target.querySelectorAll('input[type=\'checkbox\']')).filter(element => !element.parentElement.matches('checkbox-all'));
 
         for (let checkbox of filtered) {
             if (e.target.checked) {
