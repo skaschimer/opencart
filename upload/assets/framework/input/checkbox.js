1692d9f87b43ce38665b3590bb63045a915491de
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-24 18:19:38 +0800

    Update checkbox.js

--- upload/assets/framework/input/checkbox.js
+++ upload/assets/framework/input/checkbox.js
@@ -5,7 +5,7 @@ const language = loader.language('default');
 
 customElements.define('checkbox-all', class extends WebComponent {
     render() {
-        return '<input type="checkbox" id="' + this.getAttribute('input-id') + '" class="form-check-input" data-on="change:onChange" data-target="' + this.getAttribute('target') + '"/>';
+        return '<input type="checkbox"' + (this.hasAttribute('input-id') ? ' id="' + this.getAttribute('input-id') + '"' : '') + ' class="form-check-input" data-on="change:onChange" data-target="' + this.getAttribute('target') + '"/>';
     }
 
     onChange(e) {
