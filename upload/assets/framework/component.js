38d23618eb0edb2b08659e29cf96f108f4a79bf0
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-26 02:41:20 +0800

    Update component.js

--- upload/assets/framework/component.js
+++ upload/assets/framework/component.js
@@ -13,19 +13,12 @@ import('./component/pagination.js');
 // Custom
 import('./component/price.js');
 import('./component/currency.js');
-import('./component/country.js');
-import('./component/zone.js');
 
 // Input
 import('./input/autocomplete.js');
 import('./input/checkbox.js');
 //import('./input/ckeditor.js');
-//import('./input/date.js');
-//import('./input/datetime.js');
+import('./input/country.js');
+import('./input/zone.js');
 //import('./input/file.js');
-//import('./input/radio.js');
-//import('./input/select.js');
 import('./input/switch.js');
-//import('./input/text.js');
-//import('./input/textarea.js');
-//import('./input/time.js');
