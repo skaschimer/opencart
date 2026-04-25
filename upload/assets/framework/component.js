57747ed605ce207f6c005c721e513750804ac0fb
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-26 02:40:26 +0800

    Update component.js

--- upload/assets/framework/component.js
+++ upload/assets/framework/component.js
@@ -5,6 +5,7 @@ export { WebComponent } from './library/webcomponent.js';
 import('./component/alert.js');
 import('./component/button.js');
 import('./component/date.js');
+import('./component/link.js');
 import('./component/modal.js');
 import('./component/include.js');
 import('./component/pagination.js');
