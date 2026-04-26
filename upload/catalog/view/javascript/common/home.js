71c14bc7e52bb5e9f1ac3af8374af087516f24bc
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-27 00:44:07 +0800

    Update home.js

--- upload/catalog/view/javascript/common/home.js
+++ upload/catalog/view/javascript/common/home.js
@@ -2,8 +2,7 @@ import { Controller } from '../component.js';
 import { loader } from '../index.js';
 
 export default class extends Controller {
-    async render() {
-
-        return loader.template('common/home', { ...data, ...language });
+    render() {
+        return loader.template('common/home');
     }
 }
