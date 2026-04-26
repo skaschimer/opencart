802a16a9e4fa56b554dc2af720d3bf0201aeb49a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-27 00:43:50 +0800

    Update ckeditor.js

--- upload/assets/framework/input/ckeditor.js
+++ upload/assets/framework/input/ckeditor.js
@@ -1,32 +1,29 @@
 import { WebComponent } from '../library/webcomponent.js';
 import CustomEditor from './../../ckeditor/ckeditor.js';
 
-class XCkeditor extends WebComponent {
+customElements.define('x-ckeditor', class extends WebComponent {
     element = HTMLInputElement;
     editor = [];
 
-    event = {
-        connected: async () => {
-            this.editor = null;
-             //  <textarea name="category_description[{{ language.language_id }}][description]" placeholder="{{ entry_description }}" id="input-description-{{ language.language_id }}" data-oc-toggle="ckeditor" data-lang="{{ ckeditor }}" class="form-control">{{ category_description[language.language_id] ? category_description[language.language_id].description }}</textarea>
-            let editor = document.createElement('div');
+    async connected() {
+        this.editor = null;
+        //<textarea name="category_description[{{ language.language_id }}][description]" placeholder="{{ entry_description }}" id="input-description-{{ language.language_id }}" data-oc-toggle="ckeditor" data-lang="{{ ckeditor }}" class="form-control">{{ category_description[language.language_id] ? category_description[language.language_id].description }}</textarea>
+        let editor = document.createElement('div');
 
-            CustomEditor.create(editor, {
-                // Optional: Configure your editor here
-            })
+        CustomEditor.create(editor, {
+            // Optional: Configure your editor here
+        });
 
-            this.innerHTML = editor;
+        return editor;
+    }
 
-        },
-        onloaded: (countries) => {
-
-        },
-        disconnect: () => {
-            if (this.editor) {
-                this.editor.destroy();
-            }
+    disconnect() {
+        if (this.editor) {
+            this.editor.destroy();
         }
-    };
-}
+    }
 
-customElements.define('x-ckeditor', XCkeditor);
+    render() {
+
+    }
+});
