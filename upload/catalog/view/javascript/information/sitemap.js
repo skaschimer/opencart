39d6bc9f5e9c68ff3490d8878dcf58c910464bb7
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-28 01:17:22 +0800

    Update sitemap.js

--- upload/catalog/view/javascript/information/sitemap.js
+++ upload/catalog/view/javascript/information/sitemap.js
@@ -2,13 +2,14 @@ import { Controller } from '../component.js';
 import { loader } from '../index.js';
 
 // Language
-const language = await loader.language('common/language');
+const language = await loader.language('information/sitemap');
 
 export default class extends Controller {
     render() {
+        let data = {};
 
+        data.categories = {};
 
-
-        return loader.template('information/sitemap', language);
+        return loader.template('information/sitemap', { ...data, ...language });
     }
 }
