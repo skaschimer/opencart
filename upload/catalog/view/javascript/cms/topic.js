0775215cec6c05ba5bfaddfcf1a5ba0c8478af4e
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-27 22:25:03 +0800

    Update topic.js

--- upload/catalog/view/javascript/cms/topic.js
+++ upload/catalog/view/javascript/cms/topic.js
@@ -1,31 +1,51 @@
 import { Controller } from '../component.js';
+import {loader} from "../../../../assets/framework";
 
 export default class extends Controller {
     async connected() {
 
     }
+
+    async render() {
+        let data = {};
+
+        //let topic = await loader.storage('cms/topic-' + this.getAttribute('topic_id'));
+
+        //if (topic.length) {
+         //   data.name = topic.name;
+          //  data.description = topic.description;
+          //  data.image = topic.image;
+
+            //let articles = await loader.storage('cms/topic-' + this.getAttribute('topic_id'));
+
+        //}
+
+        console.log('hghg');
+
+        return await loader.template('cms/article', { ...data, ...config });
+    }
+
+    async onSubmit() {
+        let url = 'index.php?route=cms/topic&language={{ language }}';
+
+        var search = $('#input-search').val();
+
+        if (search) {
+            url += '&search=' + encodeURIComponent(search);
+        }
+
+        var topic_id = $('#input-topic').prop('value');
+
+        if (topic_id > 0) {
+            url += '&topic_id=' + topic_id;
+        }
+
+        location = url;
+    }
+
+    onKeyDown() {
+        if (e.keyCode == 13) {
+            $('#button-search').trigger('click');
+        }
+    }
 }
-
-$('#button-search').bind('click', function() {
-    url = 'index.php?route=cms/topic&language={{ language }}';
-
-    var search = $('#input-search').val();
-
-    if (search) {
-        url += '&search=' + encodeURIComponent(search);
-    }
-
-    var topic_id = $('#input-topic').prop('value');
-
-    if (topic_id > 0) {
-        url += '&topic_id=' + topic_id;
-    }
-
-    location = url;
-});
-
-$('#input-search').bind('keydown', function(e) {
-    if (e.keyCode == 13) {
-        $('#button-search').trigger('click');
-    }
-});
