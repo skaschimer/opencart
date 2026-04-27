f064fbb589c9de3e21e3916530a622e2ad789642
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-28 01:16:36 +0800

    Update webcomponent.js

--- upload/assets/framework/library/webcomponent.js
+++ upload/assets/framework/library/webcomponent.js
@@ -1,14 +1,14 @@
 export class WebComponent extends HTMLElement {
     constructor() {
         super();
-    }
 
-    async connectedCallback() {
         // Adds reactive component event changes to the attributes of the element to re-render the contents.
         for (let attribute of this.attributes) {
             this.addEventListener('[' + attribute.name + ']', this.update.bind(this));
         }
+    }
 
+    async connectedCallback() {
         if (this.connected !== undefined) {
             this.connected();
         }
